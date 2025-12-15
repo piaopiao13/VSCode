ae5a0a1e1aa958e346509a834583266f35b50969
Sarah Home PC <cys13shan@163.com>
2025-12-15 23:01:17 +0800

    根组件 App.vue

--- Vue/15-vue/src/App.vue
+++ Vue/15-vue/src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box1">
+      <div class="box2" @click="fn"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // 导出的是当前组件的配置项
+  // 里面可以提供 data（特殊）、methods、computes、watch  生命周期八大钩子
+  // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+  created() {
+    console.log('App 创建了')
+  },
+  methods: {
+    fn() {
+      alert('点击了小盒子')
+    }
   }
 }
 </script>
@@ -24,5 +35,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.box1 {
+  width: 200px;
+  height: 200px;
+  background-color: lightblue;
+  margin: 0 auto;
+  position: relative;
+}
+
+.box2 {
+  width: 100px;
+  height: 100px;
+  background-color: coral;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
