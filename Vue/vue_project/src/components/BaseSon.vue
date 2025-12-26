5922ced526839fc3cfaf44f23231b928081a19b6
Sarah Home PC <cys13shan@163.com>
2025-12-26 20:22:56 +0800

    父子组件通信

---
+++ Vue/vue_project/src/components/BaseSon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="border:1px solid red;margin:10px;padding:10px">
+        Son: {{ msg }}
+        <button @click="handleClick">Click me</button>
+    </div>
+</template>
+<script>
+export default {
+    props: ['msg'],
+    methods: {
+        handleClick() {
+            // $emit触发事件，通知父组件修改msg
+            this.$emit('updateMsg', 'Hello from BaseSon.vue')
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
