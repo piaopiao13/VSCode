b3e9d7790e4bd4ccc1ae9dad33e42ac5d1586586
Sarah Home PC <cys13shan@163.com>
2026-04-07 22:53:24 +0800

    vue3, setup

---
+++ Vue/vue3-demo/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+// setup
+// 1. 执行时机，比 beforeCreate 还早
+// 2. setup函数中获取不到this this=> undefined
+// 3. 数据＆ 函数，需要在 setup 中 return 出来，才能在模板中使用
+// export default {
+//   setup() {
+//     console.log("1. setup");
+
+//     // 数据
+//     const msg = "Hello Vue 3";
+
+//     // 函数
+//     const logMessage = () => {
+//       console.log(msg);
+//     };
+
+//     // 返回数据和函数，使其在模板中可用
+//     return {
+//       msg,
+//       logMessage,
+//     };
+//   },
+//   beforeCreate() {
+//     console.log("2. App beforeCreate");
+//   },
+// };
+//
+</script>
+
+// 4. 可以通过setup语法糖简化代码
+<script setup>
+// 数据
+const msg = "Hello Vue 3";
+
+// 函数
+const logMessage = () => {
+  console.log(msg);
+};
+</script>
+
+<template>
+  <h1>Vue 3 Composition API</h1>
+  <p>{{ msg }}</p>
+  <button @click="logMessage">click me</button>
+</template>
