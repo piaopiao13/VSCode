18679a427006614fb5e6e71fb99d4a53c7e05836
Sarah Home PC <cys13shan@163.com>
2026-04-07 22:53:57 +0800

    reactive, ref

--- Vue/vue3-demo/src/App.vue
+++ Vue/vue3-demo/src/App.vue
@@ -1,46 +1,37 @@
-<script>
-// setup
-// 1. 执行时机，比 beforeCreate 还早
-// 2. setup函数中获取不到this this=> undefined
-// 3. 数据＆ 函数，需要在 setup 中 return 出来，才能在模板中使用
-// export default {
-//   setup() {
-//     console.log("1. setup");
+<!-- <script setup>
+// 导入
+import { reactive } from 'vue'
 
-//     // 数据
-//     const msg = "Hello Vue 3";
+// 定义响应式数据
+const state = reactive({
+  count: 100,
+});
 
-//     // 函数
-//     const logMessage = () => {
-//       console.log(msg);
-//     };
+console.log(state.count) // 100
+console.log(state)
 
-//     // 返回数据和函数，使其在模板中可用
-//     return {
-//       msg,
-//       logMessage,
-//     };
-//   },
-//   beforeCreate() {
-//     console.log("2. App beforeCreate");
-//   },
-// };
-//
-</script>
+const addCount = () => {
+  state.count++
+};
+</script> 
 
-// 4. 可以通过setup语法糖简化代码
+<template>
+  <p>Count: {{ state.count }}</p>
+  <button @click="addCount">+1</button>
+</template> -->
+
 <script setup>
-// 数据
-const msg = "Hello Vue 3";
+import { ref } from "vue";
+const count = ref(100);
 
-// 函数
-const logMessage = () => {
-  console.log(msg);
+console.log(count.value); // 100
+console.log(count);
+const addCount = () => {
+  count.value++;
 };
 </script>
 
 <template>
-  <h1>Vue 3 Composition API</h1>
-  <p>{{ msg }}</p>
-  <button @click="logMessage">click me</button>
+  <p>Count: {{ count }}</p>
+  <button @click="addCount">+1</button>
 </template>
