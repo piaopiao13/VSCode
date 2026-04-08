728e446fbf233e4cb46e5c587a0cb228559b5787
Sarah Home PC <cys13shan@163.com>
2026-04-08 16:32:34 +0800

    生命周期

--- Vue/vue3-demo/src/App.vue
+++ Vue/vue3-demo/src/App.vue
@@ -1,110 +1,28 @@
 <script setup>
-import { ref, watch } from "vue";
+import { onMounted } from "vue";
 
-const count = ref(0);
+// beforeCreate 和 created 的相关代码
+// 一律放在 setup 中执行
 
-const incrementCount = () => {
-  count.value++;
+const getList = () => {
+  setTimeout(() => {
+    console.log("发送请求，获取数据");
+  }, 2000);
 };
+// 一进入页面的请求
+getList();
 
-// 1. 监视单个数据的变化
-//    watch(ref对象, (newValue, oldValue) => { ... })
-watch(count, (newValue, oldValue) => {
-  console.log(`count changed from ${oldValue} to ${newValue}`);
+// 如果有些代码需要在mounted生命周期中执行
+onMounted(() => {
+  console.log("mounted生命周期函数 - 逻辑1");
 });
 
-// 2. 监视多个数据的变化
-//    watch([ref对象1, ref对象2], (newArr, oldArr) => { ... })
-const count2 = ref(0);
-const nickname = ref("Vue3");
-const incrementCount2 = () => {
-  count2.value++;
-};
-const changeNickname = () => {
-  nickname.value = nickname.value === "Vue3" ? "Vue.js" : "Vue3";
-};
-watch([count2, nickname], (newArr, oldArr) => {
-  console.log(`count2 changed from ${oldArr[0]} to ${newArr[0]}`);
-  console.log(`nickname changed from ${oldArr[1]} to ${newArr[1]}`);
+// 写成函数的调用方式，可以调用多次，并不会冲突，而是按照顺序依次执行
+onMounted(() => {
+  console.log("mounted生命周期函数 - 逻辑2");
 });
-
-// 3. immediate 立刻执行
-const count3 = ref(0);
-const incrementCount3 = () => {
-  count3.value++;
-};
-watch(
-  count3,
-  (newValue, oldValue) => {
-    console.log(`count3 changed from ${oldValue} to ${newValue}`);
-  },
-  { immediate: true },
-);
-
-// 4. deep 深度监视（监视对象内部的变化）
-const userInfo = ref({
-  name: "zs",
-  age: 18,
-});
-const changeUserInfo = () => {
-  userInfo.value.name = userInfo.value.name === "zs" ? "ls" : "zs";
-  userInfo.value.age = userInfo.value.age === 18 ? 20 : 18;
-};
-watch(
-  userInfo,
-  (newValue, oldValue) => {
-    console.log("userInfo changed");
-    console.log("oldValue: ", oldValue);
-    console.log("newValue: ", newValue);
-    // 注意：在嵌套的变更中，
-    // 只要没有替换对象本身，
-    // 那么这里的 `newValue` 和 `oldValue` 相同
-  },
-  { deep: true },
-);
-
-// 5. 对于对象中的单个属性，进行监视
-const userInfo2 = ref({
-  name: "zs",
-  age: 18,
-});
-const changeUserInfo2 = () => {
-  userInfo2.value.name = userInfo2.value.name === "zs" ? "ls" : "zs";
-};
-watch(
-  () => userInfo2.value.name,
-  (newValue, oldValue) => {
-    console.log(`userInfo2.name changed from ${oldValue} to ${newValue}`);
-  },
-);
 </script>
 
 <template>
-  <div>
-    {{ count }} -
-    <button @click="incrementCount">+1</button>
-  </div>
-  <hr />
-  <div>
-    {{ count2 }} -
-    <button @click="incrementCount2">+1</button>
-    <br />
-    {{ nickname }} -
-    <button @click="changeNickname">Change Nickname</button>
-  </div>
-  <hr />
-  <div>
-    {{ count3 }} -
-    <button @click="incrementCount3">+1</button>
-  </div>
-  <hr />
-  <div>
-    {{ userInfo }} -
-    <button @click="changeUserInfo">Change User Info</button>
-  </div>
-  <hr />
-  <div>
-    {{ userInfo2 }} -
-    <button @click="changeUserInfo2">Change User Info 2</button>
-  </div>
+  <h1>生命周期函数</h1>
 </template>
